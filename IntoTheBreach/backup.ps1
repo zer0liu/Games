33bb6cafe0f37f03d4f27c6dd609961397781582
H. Liu <26947223+zer0liu@users.noreply.github.com>
2021-11-13 19:26:45 +0800

    Add files via upload

---
+++ IntoTheBreach/backup.ps1
@@ -0,0 +1,21 @@
+﻿# $curDateTime = Get-Date -Format "yyyyMMdd-HHmm"
+
+# $curDateTime
+
+# $curYear = $curDateTime.substring(0,4)
+#$curMonth = $curDateTime.substring(5,2)
+# $curDate = $curDateTime.substring(8,2)
+
+# $curHour = $curDateTime.substring(11,2)
+# $curMinute = $curDateTime.substring(14,2)
+
+$bakFilename = Get-Date -Format "yyyyMMdd-HHmm"
+
+$bakFilename = $bakFilename + ".bak"
+
+'[NOTE] Backup filename: "' + $bakFilename + '"'
+
+# Copy saveData.lua to backup file
+Copy-Item 'saveData.lua' $bakFilename
+
+"[Done]"
---
+++ IntoTheBreach/restore.ps1
@@ -0,0 +1,11 @@
+﻿#
+#  Restore backup file to "saveData.lua"
+#
+
+# Find the most recent backup file
+$recentBakFile = dir *.bak | Sort-Object LastAccessTime -Descending | Select-Object -First 1
+
+'[NOTE] Restore "' + $recentBakFile.name + '" to "saveData.lua"'
+copy $recentBakFile.name saveData.lua
+
+'[DONE]'
